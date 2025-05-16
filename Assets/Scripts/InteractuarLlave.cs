bbc2df68a84feb225eef8c0280d81c3e9294a845
iris-nr <irisnarru@gmail.com>
2025-05-16 13:23:31 +0200

    recoger llave

--- Assets/Scripts/InteractuarLlave.cs
+++ Assets/Scripts/InteractuarLlave.cs
@@ -1,29 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
-
-public class InteractuarNotas : MonoBehaviour
+public class InteractuarLlave : MonoBehaviour
 {
     public GameObject textCanvas;          
-    public TextMeshProUGUI textoNotaUno;  
+    public TextMeshProUGUI textoInfoLLave;  
     public float distanciaMaxima = 3f;    
 
     private bool mostrandoContenido = false;
-    private GameObject notaActual = null;
+    private GameObject LlaveActual = null;
+    public static bool tieneLlave = false;
 
     void Update()
     {
-
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         Debug.DrawRay(ray.origin, ray.direction * distanciaMaxima, Color.red);
 
         if (Physics.Raycast(ray, out RaycastHit hit, distanciaMaxima))
         {
-            if (hit.collider.CompareTag("Nota"))
+            if (hit.collider.CompareTag("Llave"))
             {
-                notaActual = hit.collider.gameObject;
+                LlaveActual = hit.collider.gameObject;
 
                 if (Input.GetKeyDown(KeyCode.E))
                 {
@@ -32,27 +32,31 @@ public class InteractuarNotas : MonoBehaviour
                     if (textCanvas != null)
                         textCanvas.SetActive(mostrandoContenido);
 
-             
+                    tieneLlave = true;
+
                     Time.timeScale = mostrandoContenido ? 0f : 1f;
+
+                    if (!mostrandoContenido && LlaveActual != null)
+                    {
+                        Destroy(LlaveActual);
+                    }
                 }
             }
             else
             {
-                notaActual = null;
+                LlaveActual = null;
             }
         }
         else
         {
-            notaActual = null;
+            LlaveActual = null;
         }
 
-
-        if (notaActual == null && textCanvas != null && textCanvas.activeSelf)
+        if (LlaveActual == null && textCanvas != null && textCanvas.activeSelf)
         {
             textCanvas.SetActive(false);
             mostrandoContenido = false;
             Time.timeScale = 1f;
-            
-        }
-    }
+        }
+    }
 }
