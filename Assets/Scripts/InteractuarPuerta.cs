8c03f77ba96b4a5a4c068329c9d9470c614eb3a9
iris-nr <irisnarru@gmail.com>
2025-05-16 19:10:34 +0200

    la puerta se abre con la llave

---
+++ Assets/Scripts/InteractuarPuerta.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractuarPuerta : MonoBehaviour
+{
+    public float distanciaMaxima = 5f;
+    public GameObject mensajeNecesitasLlave;
+    public Transform Door_A;
+
+    public Animator animator;
+    private bool PuertaDingus = false;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator no encontrado en Door_A");
+        }
+    }
+
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * distanciaMaxima, Color.green);
+
+
+        if (Physics.Raycast(ray, out hit, distanciaMaxima))
+        {
+
+            if (hit.collider.gameObject == this.gameObject)
+            {
+
+                if (!InteractuarLlave.tieneLlave && mensajeNecesitasLlave != null)
+                    mensajeNecesitasLlave.SetActive(true);
+
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (InteractuarLlave.tieneLlave)
+                    {
+                        Debug.Log("Tienes llave. Puerta abierta.");
+                        if (!PuertaDingus && animator != null)
+                        {
+                            animator.SetBool("PuertaDingus", true);
+                            PuertaDingus = true;
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("No tienes llave");
+                    }
+                }
+            }
+            else
+            {
+
+                if (mensajeNecesitasLlave != null && mensajeNecesitasLlave.activeSelf)
+                    mensajeNecesitasLlave.SetActive(false);
+            }
+        }
+        else
+        {
+
+            if (mensajeNecesitasLlave != null && mensajeNecesitasLlave.activeSelf)
+                mensajeNecesitasLlave.SetActive(false);
+        }
+    }
+
+}
+    
